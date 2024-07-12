512489cfd395a3b815aa1271745b33b465e1a080
Sapov <sapov@mail.ru>
2024-07-12 15:01:09 +0300

    login.html

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 
 class Role(models.TextChoices):
@@ -14,3 +16,15 @@ class Role(models.TextChoices):
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, unique=True)
+    role = models.CharField(
+        max_length=24, choices=Role.choices, default=Role.CUSTOMER_RETAIL
+    )
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+    )
+
+    email_verify = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
