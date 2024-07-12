ca7dfdb708cb048aa5821d8370edf1d6132a3e69
Sapov <sapov@mail.ru>
2024-07-12 17:27:08 +0300

    add test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from users.views import Register
+
+
+class LoginPageTests(TestCase):
+    def setUp(self):
+        url = reverse('login')
+        self.response = self.client.get(url)
+
+    def test_login_page(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template_loginpage(self):
+        self.assertTemplateUsed(self.response, 'registration/login.html')
+        self.assertContains(self.response, 'Login')
+
+    def test_signup_view(self):  # new
+        view = resolve('/users/login/')
+
+        self.assertEqual(view.func.__name__,
+                         Register.as_view().__name__)
