8e6e2c1354373c10c12aa5a1b09dcfcd5a4fcab3
Sapov <sapov@mail.ru>
2024-07-16 23:18:12 +0300

    functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -29,13 +29,6 @@ class NewVisitorTests(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    # def test_new_visitor_can_sign_up(self):
-    #     self.browser.find_element_by_id("id_username").send_keys("test")
-    #     self.browser.find_element_by_id("id_email").send_keys("test@test.com")
-    #     self.browser.find_element_by_id("id_password1").send_keys("test")
-    #     self.browser.find_element_by_id("id_password2").send_keys("test")
-    #     self.browser.find_element_by_id("id_submit").click()
-
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
