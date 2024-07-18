a14dc397808b9805ed061b085ce021879060b6a3
Sapov <sapov@mail.ru>
2024-07-19 02:34:08 +0300

    edit functional test address

--- functional_tests.py
+++ functional_tests.py
@@ -26,6 +26,53 @@ class NewVisitorTests(unittest.TestCase):
         self.browser.find_element(By.CSS_SELECTOR, "[name='button']").click()
         time.sleep(3)
         self.assertIn('Дашборд', self.browser.title)  # переход в дашборд
+        # # Добавляем новый адрес доставки
+        # self.browser.get("http://127.0.0.1:8000/users/list_address/")
+        # self.browser.find_element(By.CSS_SELECTOR, "[class='text']").click()
+        # time.sleep(2)
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='region']").send_keys("Московская область")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='city']").send_keys("Краснознаменск")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='street']").send_keys("Ленина")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='house']").send_keys("66")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='floor']").send_keys("1")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='flat']").send_keys("97")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='first_name']").send_keys("Юрий")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='second_name']").send_keys("Борисович")
+        # self.browser.find_element(By.CSS_SELECTOR, "[name='phone']").send_keys("+7984455175")
+        # select = Select(self.browser.find_element(By.CSS_SELECTOR, "[name='delivery_method']"))
+        # select.select_by_value('YANDEX_DELIVERY')
+        # time.sleep(5)
+        # self.browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+        # time.sleep(3)
+
+    # def test_login_page_1(self):
+    #     'Тестирую CRUD операций с адресами, которые можно создать и изменить'
+    #     self.browser.get("http://127.0.0.1:8000/users/list_address/")
+    #     self.browser.find_element(By.CSS_SELECTOR, "[class='text']").click()
+    #     time.sleep(2)
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+class AddressDeliveryTests(unittest.TestCase):
+    def setUp(self):
+        options = Options()
+        options.add_argument("--start-maximized")
+        self.browser = webdriver.Chrome(options=options)
+
+        self.browser.get("http://127.0.0.1:8000/")
+
+    def test_page_login(self):
+        '''Проверка наличия страницы "Вход"'''
+        self.assertIn('Сервис онлайн печати баннеров', self.browser.title)
+
+    def test_login_page(self):
+        self.browser.find_element(By.CSS_SELECTOR, "[name='username']").send_keys("vasa@mail.ru")
+        self.browser.find_element(By.CSS_SELECTOR, "[name='password']").send_keys("q911ww1234")
+        self.browser.find_element(By.CSS_SELECTOR, "[name='button']").click()
+        time.sleep(3)
         # Добавляем новый адрес доставки
         self.browser.get("http://127.0.0.1:8000/users/list_address/")
         self.browser.find_element(By.CSS_SELECTOR, "[class='text']").click()
@@ -41,9 +88,11 @@ class NewVisitorTests(unittest.TestCase):
         self.browser.find_element(By.CSS_SELECTOR, "[name='phone']").send_keys("+7984455175")
         select = Select(self.browser.find_element(By.CSS_SELECTOR, "[name='delivery_method']"))
         select.select_by_value('YANDEX_DELIVERY')
-        time.sleep(5)
+        time.sleep(2)
         self.browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
         time.sleep(3)
+
+
 
     # def test_login_page_1(self):
     #     'Тестирую CRUD операций с адресами, которые можно создать и изменить'
@@ -54,6 +103,7 @@ class NewVisitorTests(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
 
 
 if __name__ == '__main__':
