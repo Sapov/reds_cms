e894b6d3eb1b482d5ce60e0c1029b356f5e68520
Sapov <sapov@mail.ru>
2024-07-19 01:56:07 +0300

    edit view

--- users/views.py
+++ users/views.py
@@ -76,27 +76,6 @@ def dashboard(request):
     return render(request, 'dashboard.html')
 
 
-# @login_require
-# def edit_profile(request):
-#     if request.method == "POST":
-#         user_form = UserEditForm(instance=request.user, data=request.POST)
-#         profile_form = ProfileEditForm(
-#             instance=request.user.profile, data=request.POST, files=request.FILES
-#         )
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user_form.save()
-#             profile_form.save()
-#         return render(request, "account/complite_edit.html")
-#     else:
-#         user_form = UserEditForm(instance=request.user)
-#         profile_form = ProfileEditForm(instance=request.user.profile)
-#     return render(
-#         request,
-#         "users/edit.html",
-#         {"user_form": user_form, "profile_form": profile_form},
-#     )
-
-
 class UserListView(LoginRequiredMixin, ListView):
     ''' Посмотреть всех юзеров'''
     template_name = "users/user_list.html"
