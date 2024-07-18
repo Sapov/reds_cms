79d7bb3822690ce9d5bdf811dd97bcf234408bd7
Sapov <sapov@mail.ru>
2024-07-18 19:49:54 +0300

    edit urls

--- users/urls.py
+++ users/urls.py
@@ -3,29 +3,14 @@ from django.views.generic import TemplateView
 
 from users.views import Register, EmailVerify, dashboard, UserListView, UserCreateView, UserUpdateView, UserDeleteView
 
-
 urlpatterns = [
-
+    # ------------------------AUTH------------------
     path('', include('django.contrib.auth.urls')),
-
-    path(
-        'invalid_verify/',
-        TemplateView.as_view(template_name='registration/invalid_verify.html'),
-        name='invalid_verify'
-    ),
-
-    path(
-        'verify_email/<uidb64>/<token>/',
-        EmailVerify.as_view(),
-        name='verify_email',
-    ),
-
-    path(
-        'confirm_email/',
-        TemplateView.as_view(template_name='registration/confirm_email.html'),
-        name='confirm_email'
-    ),
-
+    path('invalid_verify/', TemplateView.as_view(template_name='registration/invalid_verify.html'),
+         name='invalid_verify'),
+    path('verify_email/<uidb64>/<token>/', EmailVerify.as_view(), name='verify_email'),
+    path('confirm_email/', TemplateView.as_view(template_name='registration/confirm_email.html'),
+         name='confirm_email'),
     path('register/', Register.as_view(), name='register'),
     # ----------------------------CRUD  model USERS-------------------
     path('list_user/', UserListView.as_view(), name='user_list'),
@@ -33,4 +18,3 @@ urlpatterns = [
     path('user_update/<pk>', UserUpdateView.as_view(), name='user_update'),
     path('user_delete/<pk>', UserDeleteView.as_view(), name='user_delete'),
 ]
-
