c5f2e988ef6fb80dc96f9964529f65951817e43c
Sapov <sapov@mail.ru>
2024-07-19 16:06:14 +0300

    add model material

--- files/models.py
+++ files/models.py
@@ -48,3 +48,58 @@ class TypePrint(models.Model):
 
     def __str__(self):
         return self.type_print
+
+
+class Material(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text="Введите имя материала для печати",
+        verbose_name="Материал для печати",
+        blank=True,
+        null=True,
+        default=None,
+    )
+    type_print = models.ForeignKey(
+        TypePrint,
+        on_delete=models.PROTECT,
+        verbose_name="Тип печати",
+        blank=True,
+        null=True,
+        default=None,
+    )
+    price_contractor = models.FloatField(
+        max_length=100,
+        help_text="За 1 м2",
+        verbose_name="Себестоимость печати в руб.",
+        blank=True,
+        null=True,
+        default=None,
+    )  # стоимость в закупке
+    price = models.FloatField(
+        max_length=100,
+        help_text="За 1 м2",
+        verbose_name="Стоимость печати для РА в руб.",
+    )
+    price_customer_retail = models.FloatField(
+        max_length=100,
+        help_text="За 1 м2",
+        verbose_name="Стоимость печати розница в руб.",
+        null=True,
+        blank=True,
+    )
+    resolution_print = models.IntegerField(
+        help_text="разрешение для печати на материале",
+        verbose_name="DPI",
+        blank=True,
+        null=True,
+        default=None,
+    )
+    is_active = models.BooleanField(default=True, verbose_name="Активный ")
+
+    def __str__(self):
+        return f"{self.name} {self.type_print}"
+
+    class Meta:
+        verbose_name_plural = "Материалы для печати"
+        verbose_name = "Материал"
+        ordering = ["name"]
