0e9a772736e70c11af51fed7c65655d8d29c3825
Sapov <sapov@mail.ru>
2024-07-19 22:27:20 +0300

    add admin

--- files/admin.py
+++ files/admin.py
@@ -9,7 +9,64 @@ from .models import (
     UseCalculator,
 )
 
-admin.site.register(Material)
 admin.site.register(FinishWork)
-admin.site.register(Product)
 admin.site.register(TypePrint)
+
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Material._meta.fields]
+    list_editable = ["price_contractor", "price", "price_customer_retail", "is_active"]
+    list_filter = ["type_print"]
+    sortable_by = ["type_print"]
+
+    class Meta:
+        model = Material
+
+
+admin.site.register(Material, MaterialAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class UseCalculatorAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in UseCalculator._meta.fields]
+    # list_editable = ["price_contractor", "price", "price_customer_retail", "is_active"]
+    list_filter = ["created_at", 'material']
+    sortable_by = ["created_at"]
+
+    class Meta:
+        model = UseCalculator
+
+
+admin.site.register(UseCalculator, UseCalculatorAdmin)
+
+
+
+class FinishWorkAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in FinishWork._meta.fields]
+    list_editable = ["price_contractor", "price", "price_customer_retail", "is_active"]
+
+    class Meta:
+        model = FinishWork
+
+
+admin.site.register(FinishWork, FinishWorkAdmin)
+
+
+class TypePrintAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in TypePrint._meta.fields]
+
+    class Meta:
+        model = TypePrint
+
+
+admin.site.register(TypePrint, TypePrintAdmin)
+admin.site.register(StatusProduct)
