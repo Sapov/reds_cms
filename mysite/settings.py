7c6f080313b2e9fd3cb4cc3eb4be30223d710d77
Sapov <sapov@mail.ru>
2024-07-20 13:55:05 +0300

    add logging

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,14 +23,35 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "easy_thumbnails",
     'users.apps.UsersConfig',
     'files',
     'orders',
     'crispy_forms',
     "crispy_bootstrap4",
-    "easy_thumbnails",
 
 ]
+
+# _____________________LOGING________________________________
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {"format": "%(name)-12s %(levelname)-8s %(message)s"},
+        "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "console"},
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "formatter": "file",
+            "filename": "debug.log",
+        },
+    },
+    "loggers": {"": {"level": "INFO", "handlers": ["console", "file"]}},
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
